0632d8014697696d704794d346aa9ec4fb9c88c6
T0Y3 <toye.zty@gmail.com>
2018-04-21 17:17:42 +0800

    4.21 17:17

--- DataStructure/Example/eightqueens.cpp
+++ DataStructure/Example/eightqueens.cpp
@@ -1,31 +1,55 @@
-#include<iostream>
-#include<cmath>
-bool check(int chess[]);
-int main(){
-	int i,j;
-	int *p;
-	int chess[8]={1,1,1,1,1,1,1,1};
-	p=chess;
-	while()
+#include <iostream>
+using namespace std;
+
+void chessrow(int row);
+int chesscolomn(int row, int colomn);
+
+int chess[8]={0};
+int count=0;
+
+void chessrow(int row)
+{
+    while(chess[row]=chesscolomn(row,0))
+    {
+        if(row<8)
+            return chessrow(row+1);
+        else
+            row=-1;
+            count++;
+            cout<<"=========="<<endl;
+    }   
 }
 
-bool check(int chess[])
+int chesscolomn(int row, int colomn)
 {
-	int i,j;
-	for(i=0;i<8;i++)
-		for(j=8;j<0;j--)
-			if(chess[i]%8==chess[j]%8)//check colomn
-				return false;
-			else if(chess[i]==chess[j])
-			else if(chess[i]>chess[j])//check slant
-			{
-				if((chess[i]-abs(chess[i]%8-chess[j]%8)+abs(chess[i]-chess[j]))==chess[j]||(chess[i]-abs(chess[i]%8-chess[j]%8)-abs(chess[i]-chess[j]))==chess[j])
-					return false;
-			}
-			else if(chess[i]<chess[j])
-			{
-				if((chess[i]+abs(chess[i]%8-chess[j]%8)+abs(chess[i]-chess[j]))==chess[j]||(chess[i]+abs(chess[i]%8-chess[j]%8)-abs(chess[i]-chess[j]))==chess[j])
-					return false;
-			}
-	return true;
+    if(colomn<8)
+    {
+        chess[row]=colomn;
+        for(int i=0;i<row;i++)
+        {
+            if(chess[i]==chess[row])//check colomn
+                {
+                    cout<<"* ";
+                    return chesscolomn(row,colomn+1);
+                }
+            if((chess[i]+row-i==chess[row])||(chess[i]-row+i==chess[row]))//check slant
+                {
+                    cout<<"* ";
+                    return chesscolomn(row,colomn+1);
+                }
+            cout<<"# ";
+            return colomn;
+        }
+    }
+    else
+        colomn=-1;
+        cout<<endl;
+    return chesscolomn(row,colomn+1);
+}
+
+int main()
+{
+    chessrow(0);
+    cout<<"Totol:"
+        <<count<<endl;
 }
